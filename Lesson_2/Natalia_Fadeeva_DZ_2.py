047e2397e907b18248ac8792a401bcdbd6f7104a
natnut928 <73323377+natnut928@users.noreply.github.com>
2021-08-11 01:02:09 +0800

    Update and rename Natalia_Fadeeva_DZ_2.py to Lesson_2/Natalia_Fadeeva_DZ_2.py

--- Lesson_2/Natalia_Fadeeva_DZ_2.py
+++ Lesson_2/Natalia_Fadeeva_DZ_2.py
@@ -7,11 +7,11 @@ import requests
 
 count=10
 url = 'https://api.hh.ru/vacancies?per_page=10&page=1&specialization=1&area=1&text=руководитель&currency=RUR&only_with_salary=true'
-#headers = Headers(headers = True).generate()
+
 response = requests.get(url)
 r = response.json()
 print(r)
-#soup = BeautifulSoup(response.text, 'lxml')
+
 
 with open('hh_vac.csv', 'w') as f:
     f.write(",Name, Min, Max, Position URL, Site\n")
@@ -22,6 +22,4 @@ with open('hh_vac.csv', 'w') as f:
 
         f.write("{},{},{},{},{},{}\n".format(i,rec["name"],s_min,s_max, rec["alternate_url"], "http://hh.ru"))
 
-        # print(rec["name"], s_min, s_max, rec["alternate_url"], "http://hh.ru")
-print("we are done")
-#df = pd.DataFrame(vacancies_info).to_csv('DZ_2.csv')
+
